e604ce372f6b7e474b6c113998a3a7653940c317
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-01-11 23:07:10 -0800

    Complete linkedList data structure

---
+++ linkedList.js
@@ -0,0 +1,100 @@
+/*
+ * Implement a linked list using the pseudoclassical instantiation pattern.
+ *
+ * Your linked list should have methods called "addToTail", "removeHead", and "contains."
+ *
+ */
+
+// EXAMPLE USAGE:
+// var list = new LinkedList();
+// list.tail;         //yields 'null'
+// list.addToTail(4);
+// list.addToTail(5);
+// list.head.value;   //yields '4';
+// list.contains(5);  //yields 'true';
+// list.contains(6);  //yields 'false';
+// list.removeHead(); //yields '4';
+// list.tail.value;   //yields '5';
+// list.removeHead(); //yields '5';
+// list.removeHead(); //yields 'null';
+
+
+var LinkedList = function () {
+  this.head = null;
+  this.tail = null;
+};
+
+//write methods here!
+
+LinkedList.prototype.addToTail = function (value) {
+  const node = this.makeNode(value);
+
+  if (this.head === null) {
+    this.tail = this.head = node;
+  } else {
+    this.tail.next = node;
+    this.tail = node;
+  }
+};
+
+LinkedList.prototype.removeHead = function () {
+  const output = this.head;
+
+  if (this.head !== null) {
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = this.tail = null;
+    }
+  }
+  return output ? output.value : null;
+};
+
+LinkedList.prototype.contains = function (value) {
+  let node = this.head;
+  while (node) {
+    if (node.value === value) {
+      return true;
+    } else {
+      node = node.next;
+    }
+  }
+  return false;
+};
+
+LinkedList.prototype.makeNode = (value) => {
+  const node = {};
+  node.value = value;
+  node.next = null;
+  return node;
+};
+
+var list = new LinkedList();
+console.log(list.tail);         //yields 'null'
+list.addToTail(4);
+list.addToTail(5);
+console.log(list.head.value);   //yields '4';
+console.log(list.contains(5));  //yields 'true';
+console.log(list.contains(6));  //yields 'false';
+console.log(list.removeHead()); //yields '4';
+console.log(list.tail.value);   //yields '5';
+console.log(list.removeHead()); //yields '5';
+console.log(list.removeHead()); //yields 'null';
+
+const list1 = new LinkedList();
+list1.addToTail(5);
+console.log(list1.contains(5)) //true;
+console.log(list1.contains(6)) //false;
+list1.addToTail(6);
+list1.addToTail(7);
+list1.addToTail(8);
+list1.addToTail(9);
+console.log(list1);
+console.log(list1.contains(6)) //true;
+console.log(list1.removeHead()) //5;
+console.log(list1.contains(5)) //false;
+console.log(list1.contains(7)) //true;
+console.log(list1.contains(8)) //true;
+console.log(list1.contains(10)) //false;
+
+

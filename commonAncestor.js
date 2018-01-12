5553caf4c32b727546935a88a2e79527256e8d59
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-01-11 23:49:49 -0800

    Working on wordfiler & commonAncestor

---
+++ commonAncestor.js
@@ -0,0 +1,113 @@
+
+/**
+  * implement the function `getClosestCommonAncestor` and `getAncestorPath`
+  * in the following Tree class
+  */
+
+
+var Tree = function (value) {
+  this.value = value
+  this.children = [];
+};
+
+
+Tree.prototype.addChild = function (child) {
+  if (!this.isDescendant(child)) {
+    this.children.push(child);
+  } else {
+    throw new Error('That child is already a child of this tree');
+  }
+  return this;
+};
+
+/**
+  * check to see if the provided tree is already a child of this
+  * tree __or any of its sub trees__
+  */
+Tree.prototype.isDescendant = function (child) {
+  if (this.children.indexOf(child) !== -1) {
+    // `child` is an immediate child of this tree
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].isDescendant(child)) {
+        // `child` is descendant of this tree
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+Tree.prototype.getClosestCommonAncestor = function (node1, node2) {
+  //i: 2 nodes to find the most common ancestor for
+  //o: the closest common link between the nodes
+  //c: none
+  //e: null if no ancestor
+};
+
+/**
+  * return the lowest common ancestor of the two child nodes.
+  * (assume for these examples that only a women can be the parent of a child)
+  * more examples:
+  *  1.) between me and my brother -> my mom
+  *  2.) between me and my cousin -> my grandma
+  *  3.) between my grandma and my grandma -> my grandma
+  *  4.) between me and a potato -> null
+  */
+
+
+
+
+//example usage:
+var grandma = new Tree('grandma');
+var mom = new Tree('mom');
+grandma.addChild(mom);
+var me = new Tree('me');
+mom.addChild(me);
+var momsSister = new Tree('momSister');
+grandma.addChild(momsSister);
+var cousin = new Tree('cousin');
+momsSister.addChild(cousin);
+console.log(grandma.getClosestCommonAncestor(me, cousin)); // grandma;
+
+
+/**
+  * should return the ancestral path of a child to this node.
+  * more examples:
+  * 1.) greatGrandma.getAncestorPath(me) -> [great grandma, grandma, mom, me]
+  * 2.) mom.getAncestorPath(me) -> [mom, me]
+  * 3.) me.getAncestorPath(me) -> [me]
+  * 4.) grandma.getAncestorPath(H R Giger) -> null
+  */
+Tree.prototype.getAncestorPath = function (node, result) {
+  if (!result) {
+    result = [];
+    let isRelative = this.isDescendant(node);
+    if (isRelative) {
+      result.push(this);
+    }
+  }
+  this.children.forEach(child => {
+    if (child === node || child.isDescendant(node)) {
+      result.push(child);
+      child.getAncestorPath(node, result);
+    }
+  });
+  return result;
+};
+
+console.log(grandma.getAncestorPath(me)); // => [grandma, mom, me]
+
+/**
+  * remove an immediate child
+  */
+Tree.prototype.removeChild = function (child) {
+  var index = this.children.indexOf(child);
+  if (index !== -1) {
+    // remove the child
+    this.children.splice(index, 1);
+  } else {
+    throw new Error('That node is not an immediate child of this tree');
+  }
+};
--- wordFilter.js
+++ wordFilter.js
@@ -1,14 +1,53 @@
+/* Given many words, words[i] has weight i.
+
+Design a class WordFilter that supports one function, 
+WordFilter.f(String prefix, String suffix). It will return the word with 
+given prefix and suffix with maximum weight. If no word exists, return -1.
+
+Examples:
+Input:
+WordFilter(["apple"])
+WordFilter.f("a", "e") // returns 0
+WordFilter.f("b", "") // returns -1
+Note:
+words has length in range[1, 15000].
+For each test case, up to words.length queries WordFilter.f may be made.
+  words[i] has length in range[1, 10].
+    prefix, suffix have lengths in range[0, 10].
+      words[i] and prefix, suffix queries consist of lowercase letters only.
+
+
+*/
+
+
 const WordFilter = function (words) {
-  this.words = words.sort((a, b) => a.length - b.length));
-};
+  this.words = words;
+}
 
 WordFilter.prototype.f = function (prefix, suffix) {
-  return this.words.reduceRight((index, word, i) => {
-    if (word[0] === prefix && word[word.length - 1] === suffix) {
-      index = i;
+  for (let i = this.words.length - 1; i >= 0; i--) {
+    let word = this.words[i];
+    let j = 0;
+    let k = word.length - 1;
+    let foundPrefix = foundSuffix = false;
+    while (k > j && (!foundPrefix && !foundSuffix)) {
+      if (word[j] === prefix || word[j] === ' ') foundPrefix = true;
+      if (word[k] === suffix || word[k] === ' ') foundSuffix = true;
+      k--;
+      j++;
     }
-    return index;
-  }, undefined);
-};
+    if (foundPrefix && foundSuffix) {
+      return i;
+    } else {
+      foundPrefix = foundSuffix = false;
+    }
+  }
+  return -1;
+}
 
-console.log()
+
+let example = new WordFilter(['apple']);
+let result1 = example.f('a', 'e');
+let result2 = example.f('b', '');
+console.log('result1', result1); // 0 
+console.log('result2', result2); // -1

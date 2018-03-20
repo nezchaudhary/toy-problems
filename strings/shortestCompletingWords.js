fc80de62e245babb9ab270bb135ed60b03a5153a
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-03-20 14:13:24 -0700

    Organize folders

--- strings/shortestCompletingWords.js
+++ strings/shortestCompletingWords.js
@@ -1,3 +1,17 @@
+/*
+
+Find the minimum length word from a given dictionary words, which has all the letters from the string 
+licensePlate. Such a word is said to complete the given string licensePlate
+
+Here, for letters we ignore case. For example, "P" on the licensePlate still matches "p" on the word.
+
+It is guaranteed an answer exists. If there are multiple answers, return the one that occurs first in the 
+array.
+
+The license plate might have the same letter occurring multiple times. For example, given a licensePlate 
+of "PP", the word "pair" does not complete the licensePlate, but the word "supper" does.
+
+*/
 
 const sortWord = (word) => {
   return word.split('').sort().join('').toLowerCase();

42a57ed26fc9f08b7ba3ec298a8b5eb2b64b3f7e
nehachaudhary <neha@owler-inc.com>
2019-03-30 16:43:17 -0700

    Max profit II

--- arrays/max-profit-II.js
+++ arrays/max-profit-II.js
@@ -82,7 +82,29 @@
 
 // console.log(maxProfit([7,1,5,3,6,4]));
 
-const maxProfit = prices => {
+
+
+/* 
+SOLUTION: Peak Valley Approach
+
+
+If we analyze the graph, we notice that the points of interest are the consecutive valleys and peaks.
+
+Mathematically speaking: Total Profit= \sum_{i}(height(peak_i)-height(valley_i)) 
+TotalProfit=∑i  (height(peaki)−height(valleyi))
+
+The key point is we need to consider every peak immediately following a valley to maximize the profit. In case we skip one of the peaks (trying to obtain more profit), we will end up losing the profit over one of the transactions leading to an overall lesser profit.
+
+For example, in the above case, if we skip peak_ipeak 
+i
+​	
+  and valley_jvalley 
+j
+​	
+  trying to obtain more profit by considering points with more difference in heights, the net profit obtained will always be lesser than the one obtained by including them, since CC will always be lesser than A+BA+B.
+*/
+
+const maxProfitPeakValley = prices => {
   // image a graph to plot these values and you want the valley and peak of the graphs
   // ultimately the largest difference wont matter because they will add up
 
@@ -106,6 +128,23 @@ const maxProfit = prices => {
   return maxProfit;
 }
 
+/* SOLUTION: Simple One Pass
+This solution follows the logic used in Approach 2 itself, but with only a slight variation. In this case, instead of looking for every peak following a valley, we can simply go on crawling over the slope and keep on adding the profit obtained from every consecutive transaction. In the end,we will be using the peaks and valleys effectively, but we need not track the costs corresponding to the peaks and valleys along with the maximum profit, but we can directly keep on adding the difference between the consecutive numbers of the array if the second number is larger than the first one, and at the total sum we obtain will be the maximum profit. This approach will simplify the solution. This can be made clearer by taking this example:
 
-console.log(maxProfit([7,1,5,3,6,4]));
+[1, 7, 2, 3, 6, 7, 6, 7]
+
+The graph corresponding to this array is:
+*/
+const maxProfitSimpleOnePass = prices => {
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1])
+      maxProfit += prices[i] - prices[i - 1];
+  }
+  return maxProfit;
+};
+
+
+console.log(maxProfitPeakValley([7,1,5,3,6,4]));
+console.log(maxProfitSimpleOnePass([7,1,5,3,6,4]));
 console.log(maxProfit([7,1,0,0,2]));

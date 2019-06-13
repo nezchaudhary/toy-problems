00abb9a12dda92b7593e5975fc43d28de8b83012
nehachaudhary <neha@owler-inc.com>
2019-06-12 22:30:17 -0700

    Level order traversal

--- trees/levelOrderTraversal.js
+++ trees/levelOrderTraversal.js
@@ -21,12 +21,24 @@ return its level order traversal as:
 */
 
 const levelOrder = (root, result = [], depth = 0) => {
+  // it is an empty tree
   if (root === null) return [];
+
+  // root level. Add at 0 index
   if (result.length === 0) result[depth++] = [root.val];
+
+  // No other nodes except root
   if (root.left === null && root.right === null) return result;
+
+  //
   if (result[depth] === undefined) result[depth] = [];
-  if (root.left) result[depth].push(root.left.val);
-  if (root.right) result[depth].push(root.right.val);
+
+  // Depth is now already at index to push value at. Push left value
+  if (root.left) result[depth].push(root.left.val); // shouldn't we increment?
+
+  // push right value
+  if (root.right) result[depth].push(root.right.val); // increment??
+  
   levelOrder(root.left, result, ++depth);
   levelOrder(root.right, result, depth);
   return result;

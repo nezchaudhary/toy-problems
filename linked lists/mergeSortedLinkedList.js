c39bd96454076922b98a01c3ba3f2557324e61d5
nehachaudhary <neha@owler-inc.com>
2019-09-21 23:07:47 -0700

    Finish merge sorted linked lst

--- linked lists/mergeSortedLinkedList.js
+++ linked lists/mergeSortedLinkedList.js
@@ -17,6 +17,13 @@ Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+const ListNode = (val) => {
+  this.val = val;
+  this.next = null;
+};
+
+
 const mergeTwoLists = (l1, l2) => {
   if (l1 === null && l2 === null) return null;
   let list;
@@ -60,3 +67,37 @@ const mergeTwoLists = (l1, l2) => {
   }
   return list;
 };
+
+const mergeTwoListsRecursive = (l1, l2) => {
+  if (l1 === null) {
+    return l2;
+  } else if (l2 === null) {
+    return l1;
+  } else if (l1.val < l2.val) {
+    l1.next = mergeTwoListsRecursive(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoListsRecursive(l1, l2.next);
+    return l2;
+  }
+}
+
+const mergeTwoListsIterative = (l1, l2) => {
+  const preHead = new ListNode(-1);
+  let prev = preHead;
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      prev.next = l1;
+      l1 = l1.next;
+    } else {
+      prev.next = l2;
+      l2 = l2.next;
+    }
+    prev = prev.next;
+  }
+  prev.next = l1 === null ? l2 : l1;
+  return preHead.next;
+}
+
+// input [1, 2, 4], [1, 3, 4] 
+// return [1, 1, 2, 3, 4, 4] 

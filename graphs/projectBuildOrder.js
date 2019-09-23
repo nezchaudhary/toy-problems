3dfdee172ae8a7a3e0769674f4252cf5ae1361c8
nehachaudhary <neha@owler-inc.com>
2019-09-22 19:24:28 -0700

    Project builder

--- graphs/projectBuildOrder.js
+++ graphs/projectBuildOrder.js
@@ -1,3 +1,21 @@
+/*
+
+CTCI - Trees & Graphs 4.7
+
+Topological Sort
+
+Build Order: You are given a list of projects and a list of dependencies (which is a list of pairs of projects where the second project is dependent on the first project.) All of a project's dependencies must be built before the project is. Find a build order that will allow the projects to be built. If there is no valid build order, return an error.
+
+EXAMPLE:
+  Input:
+    projects: a, b, c, d, e, 
+    dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
+Output:
+  f, e, a, b, d, c
+
+*/
+
+// Common to Solutions
 // Project class
 class Project {
   constructor(name) {
@@ -5,37 +23,19 @@ class Project {
     this.map = {}; // edges
     this.name = name;
     this.dependencies = 0;
+    this.state = 'blank';
   }
 
   addNeighbor(node) {
     if(!this.map[node]) {
       this.children.push(node);
-      this.map[node.getName()] =  node;
-      node.incrementDependencies();
+      this.map[node.name] =  node;
+      node.dependencies++;
     }
-  }
-
-  incrementDependencies() {
-    this.dependencies++;
-  }
-
-  decrementDependencies() {
-    this.dependencies--;
-  }
-
-  getName() {
-    return this.name;
-  }
-
-  getChildren() {
-    return this.children;
-  }
-
-  getNumberDependencies() {
-    return this.dependencies;
   }
 }
 
+// Common to Solutions
 // Graph Class
 class Graph {
   constructor() {
@@ -45,8 +45,8 @@ class Graph {
 
   getOrCreateNode(name) {
     if (!this.map[name]) {
-      node = new Project(name);
-      nodes.push(node);
+      const node = new Project(name);
+      this.nodes.push(node);
       this.map[name] = node;
     }
     return this.map[name];
@@ -57,16 +57,13 @@ class Graph {
     let end = this.getOrCreateNode(endName);
     start.addNeighbor(end);
   } 
-  
-  getNodes() {
-    return this.nodes;
-  }
 }
 
-// Helper function to insert projects with zero dependencies into the order array starting at offset index
+// Common to Solutions
+// Helper function to insert projects with zero dependencies into the order array starting at offset index 
 const addNonDependent = (order, projects, offset) => {
   for (let project of projects) {
-    if (project.getNumberDependencies() === 0) {
+    if (project.dependencies === 0) {
       order[offset] = project;
       offset++;
     }
@@ -74,7 +71,7 @@ const addNonDependent = (order, projects, offset) => {
   return offset;
 }
 
-
+// Common to Solutions
 // function that creates a graph creating edges a to b if b is dependent on a.
 const buildGraph = (projects, dependencies) => {
   let graph = new Graph();
@@ -90,6 +87,9 @@ const buildGraph = (projects, dependencies) => {
   return graph;
 }
 
+//------------------------------------------- //
+
+// Solution 1
 // Return a list of the projects correct build order
 const orderProjects = (projects) => {
   let order = new Array(projects.length); // total number of projects
@@ -102,21 +102,64 @@ const orderProjects = (projects) => {
       return null;
     }
 
-    let children = current.getChildren();
+    let children = current.children;
     for (let child of children) {
-      child.decrementDependencies();
+      child.dependencies--;
     }
 
     endOfList = addNonDependent(order, children, endOfList);
     toBeProcessed++;
   }
   return order;
-
 }
-
 
 // main function call to problem
 const findBuildOrder = (projects, dependencies) => {
   let graph = buildGraph(projects, dependencies);
-  return orderProjects(graph.getNodes());
+  return orderProjects(graph.nodes);
 };
+
+
+//-------------------------------------------------//
+
+// Solution 2
+// DFS
+const doDFS = (project, stack) => {
+  if (project.state === 'partial') {
+    return false; // Cycle
+  }
+
+  if (project.state === 'blank') {
+    project.state = 'partial';
+    const children = project.children;
+    for (let child = 0; child < children.length; child++) {
+      if (!doDFS(child, stack)) {
+        return false;
+      }
+    }
+    project.state = 'complete';
+    stack.push(project);
+  }
+  return true;
+}
+
+
+const orderProjectsDFS = projects => {
+  const stack = new Array();
+  for (let project of projects) {
+    if (project.state === 'blank') {
+      if (!doDFS(project, stack)) {
+        return null;
+      }
+    }
+  }
+  return stack;
+}
+
+const findBuildOrderDFS = (projects, dependencies) => {
+  let graph = buildGraph(projects, dependencies);
+  return orderProjectsDFS(graph.nodes);
+}
+
+console.log('Normal', findBuildOrder([1, 0, 2, 3], [[0, 1], [0, 2], [1, 3], [2, 3]])) // [0, 1, 2, 3]
+console.log('DFS', findBuildOrderDFS([1, 0, 2, 3], [[0, 1], [0, 2], [1, 3], [2, 3]])); //// [0, 1, 2, 3]

b0304790beba9967e957d8f3f51da29976fabd90
nehachaudhary <neha@owler-inc.com>
2019-10-08 00:52:04 -0700

    Finish and move telephone words

---
+++ recursion/telephoneWords.js
@@ -0,0 +1,62 @@
+/*
+  * Each number key on a standard phone keypad has a set of Latin letters written on
+  * it as well: http://en.wikipedia.org/wiki/File:Telephone-keypad2.svg
+  *
+  * Businesses often try to come up with clever ways to spell out their phone number
+  * in advertisements to make it more memorable. But there are a lot of combinations!
+  *
+  * Write a function that takes up to four digits of a phone number, and
+  * returns a list of all of the words that can be written on the phone with
+  * that number. (You should return all permutations, not only English words.)
+  *
+  * Example:
+  *   telephoneWords('2745');
+  *   => ['APGJ',
+  *        'APGK',
+  *        'APGL',
+  *        ..., // many many more of these
+  *        'CSIL']
+  *
+  * Tips:
+  *   - Phone numbers are strings! (A phone number can start with a zero.)
+  *   - The digits 0 and 1 do not have letters associated with them, so they should be left as numbers.
+  *   - Don't return every combination of those digits in any order, just the order given.
+  *
+  *  Extra credit: There's a list of English dictionary words at /usr/share/dict/words .
+  *  Why not filter your results to only return words contained in that file?
+  *
+  */
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+const combinations = {
+  2: 'abc',
+  3: 'def',
+  4: 'ghi',
+  5: 'jkl',
+  6: 'mno',
+  7: 'pqrs',
+  8: 'tuv',
+  9: 'wxyz'
+};
+
+const telephoneWords = (digits, index = 0, word = '', result = []) => {
+  if (digits.length === 0) return [];
+  if (index === digits.length) {
+    result.push(word);
+    return;
+  }
+
+  const alphabets = combinations[digits[index]];
+  for (let j = 0; j < alphabets.length; j++) {
+    if (index < digits.length) {
+      telephoneWords(digits, index + 1, word + alphabets[j], result);  
+    }
+  }  
+  return result;
+};
+
+console.log(telephoneWords('22'));

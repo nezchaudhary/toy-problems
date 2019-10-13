6238056404ba5502575e8ebf5b4265d5ff373506
nehachaudhary <neha@owler-inc.com>
2019-10-13 16:01:35 -0700

    Clean up quick sort

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -1,34 +1,37 @@
-const swap = (items, firstIndex, secondIndex) => {
-  [items[firstIndex], items[secondIndex]] = [items[secondIndex], items[firstIndex]];
+
+
+const swap = (arr, i, j) => {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
 };
 
-const partition = (arr, pivot, left, right) => {
+const divide = (arr, pivot, left, right) => {
   let pivotValue = arr[pivot];
-  let partitionIndex = left;
+  let partition = left;
 
   for (let i = left; i < right; i++) {
     if (arr[i] < pivotValue) {
-      swap(arr, i, partitionIndex);
-      partitionIndex++;
+      swap(arr, i, partition);
+      partition++;
     }
   }
-  swap(arr, right, partitionIndex);
-  return partitionIndex;
+  swap(arr, right, partition);
+  return partition;
 }
 
 const quickSort = (arr, left = 0, right) => {
   let len = arr.length;
   if (right === undefined) right = len - 1;
   let pivot;
-  let partitionIndex;
+  let partition;
 
   if (left < right) {
     pivot = right;
-    partitionIndex = partition(arr, pivot, left, right);
+    partition = divide(arr, pivot, left, right);
 
-  //sort left and right
-  quickSort(arr, left, partitionIndex - 1);
-  quickSort(arr, partitionIndex + 1, right);
+    //sort left and right
+    quickSort(arr, left, partition - 1);
+    quickSort(arr, partition + 1, right);
+    
   }
   return arr;
 };

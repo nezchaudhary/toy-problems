ce5ab6ce517e79eafb3b5fca9856e8a50d8d77db
nehachaudhary <neha@owler-inc.com>
2019-11-02 20:04:44 -0700

    Finish remove k digits

---
+++ stacks and queues/removeKDigits.js
@@ -0,0 +1,32 @@
+const removeDigits = function(nums, k) {
+  if (nums.length === 0) return '0';
+  if (k >= nums.length) return '0';
+  
+  const stack = [nums[0]];
+  let toRemove = k;
+  let i = 1;
+  while (i < nums.length) {
+    if (stack[stack.length - 1] > nums[i] && toRemove > 0) {
+        stack.pop();
+      toRemove--;
+      
+    } else {
+        stack.push(nums[i]);
+      i++;
+    }
+  }
+  
+  while (toRemove > 0) {
+    stack.pop();
+    toRemove--;
+  }
+  
+    let s = 0;
+    while(stack[s] === '0') s++;
+    
+    const result = stack.slice(s);
+    
+    return result.length === 0 ? '0' : result.join('');
+};
+console.log(removeDigits('1432219', 3));
+console.log(removeDigits('10200', 1));

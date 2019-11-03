dfa07c463786ad61984103c9565ed88f0aa6b237
nehachaudhary <neha@owler-inc.com>
2019-11-02 21:09:37 -0700

    FInish word ladder

--- graphs/wordLadder.js
+++ graphs/wordLadder.js
@@ -1,162 +1,154 @@
-const checkWordChange = (word1, word2) => {
+/*
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time.
+Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+Note:
+
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+You may assume no duplicates in the word list.
+You may assume beginWord and endWord are non-empty and are not the same.
+Example 1:
+
+Input:
+beginWord = "hit",
+endWord = "cog",
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+Output: 5
+
+Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+Example 2:
+
+Input:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+
+Output: 0
+
+Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+*/
+
+// Time Complexity - O(m x n)
+// Space Complexity - O (m X n)
+const isAdjacent = (w1, w2) => {
   let count = 0;
-  for (let i = 0; i < word1.length; i++) {
-    if (word1[i] !== word2[i]) count++;
+  for (let i = 0; i < w1.length; i++) {
+    if (w1[i] !== w2[i]) count++;
+    if (count > 1) break;
   }
   return count === 1;
 }
 
+const ladderLength = (beginWord, endWord, wordList) => {
+  let level = 0;
+  let seen = {};
+  let queue = [beginWord];
 
-const wordLadder = (start, end, dictionary, result = []) => {
-  if (dictionary.length === 0) return [];
-  if (start === end) {
-    result.push(end);
-    return;
-  }
-  result.push(start);
-  let word;
-  for (let i = 0; i < dictionary.length; i++) {
-    if (checkWordChange(start, dictionary[i])) {
-      word = dictionary[i];
-      dictionary.splice(i, 1);
-      break;
+  while (queue.length) {
+    let size = queue.length;
+    level++;
+    for (let i = 0; i < size; i++) {
+      let curr = queue.shift();
+      for (let j = 0; j < wordList.length; j++) {
+        let word = wordList[j];
+        if (isAdjacent(curr, word)) {
+          if (word === endWord) return level + 1;
+          if (!seen[word]) {
+            seen[word] = true;
+            queue.push(word);
+          }
+        }
+      }
     }
   }
 
-  wordLadder(word, end, dictionary, result);
-  return result;
+  return 0;
+};
 
+/* ----------------------- */
 
+// GRAPH Solution (One-directional)
+
+// Time Complexity - O(m x n)
+// Space Complexity - O (m X n)
+const ladderLengthGraphOneDirectional = (beginWord, endWord, wordList) => {
+  let wordGraph = wordList.includes(beginWord) ? createGraph(wordList) : createGraph([beginWord, ...wordList]);
+  let beginningNode = wordGraph.getNode(beginWord);
+  beginningNode.steps = 1;
+  let queue = [beginningNode];
+
+  while (queue.length) {
+    let node = queue.shift();
+    node.visited = true;
+
+    if (node.word === endWord) return node.steps;
+
+    for (let neighbor of node.neighbors) {
+      if (neighbor.visited === false) {
+        neighbor.steps = node.steps + 1;
+        neighbor.visited = true;
+        queue.push(neighbor);
+      }
+    }
+  }
+
+  return 0;
+};
+
+const createGraph  = (words) => {
+  let graph = new Graph(words);
+
+  for (let i = 0; i < words.length - 1; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      if (isAdjacent(words[i], words[j])) {
+        graph.addEdge(words[i], words[j]);
+      }
+    }
+  }
+  return graph;
 }
 
-console.log(wordLadder('TOON', 'PLEA', ['POON', 'PLEE', 'SAME', 'POIE', 'PLEA', 'PLIE', 'POIN']));
+class Graph {
+  constructor(words) {
+    this.nodes = [];
+    this.graph = {};
+    for (let word of words) {
+      this.addNode(word);
+    }
+  }
 
+  addEdge(w1, w2) {
+    const firstNode = this.getNode(w1);
+    const secondNode = this.getNode(w2);
+    firstNode.neighbors.push(secondNode);
+    secondNode.neighbors.push(firstNode);
+  }
 
-// public int ladderLength(String beginWord, String endWord, List < String > wordList) {
-//   if (!wordList.contains(endWord)) {
-//     return 0;
-//   }
+  addNode(word) {
+    this.graph[word] = new WordNode(word);
+    this.nodes.push(this.graph[word]);
+  } 
 
-//   Queue < String > q = new LinkedList<>();
-//   q.add(beginWord);
-//   Set < String > visited = new HashSet<>();
-//   int lvl = 1;
-//   Set < String > dict = new HashSet<>();
-//   for (String s : wordList) {
-//     dict.add(s);
-//   }
-
-//   while (!q.isEmpty()) {
-//     int lvlsz = q.size();
-//     for (int li = 0; li < lvlsz; li++) {
-//       String word = q.poll();
-//       if (word.equals(endWord)) {
-//         return lvl;
-//       }
-//       for (int ci = 0; ci < word.length(); ci++) {
-//         for (char c = 'a'; c <= 'z'; c++) {
-//           char[] ch = word.toCharArray();
-//           ch[ci] = c;
-//           String newWord = new String(ch);
-//           if (!visited.contains(newWord) && dict.contains(newWord)) {
-//             q.add(newWord);
-//             visited.add(newWord);
-//           }
-//         }
-//       }
-//     }
-//     lvl++;
-//   }
-
-//   return 0;
-// }
-
-
-// class GraphNode {
-//   constructor(val) {
-//     this.val = val;
-//     this.children = [];
-//   }
-
-//   addChild(node) {
-//     this.children.push(node);
-//     return this;
-//   }
-// }
-
-// class Graph {
-//   constructor() {
-//     this.nodes = new Map();
-//   }
-
-//   add(val1, val2) {
-//     const node1 = this.nodes.get(val1) || new GraphNode(val1);
-//     const node2 = this.nodes.get(val2) || new GraphNode(val2);
-//     this.nodes.set(val1, node1.addChild(node2));
-//     this.nodes.set(val2, node2.addChild(node1));
-//   }
-
-//   get(val) {
-//     return this.nodes.get(val);
-//   }
-// }
-
-// function ladderLength(beginWord, endWord, wordList) {
-//   wordList.push(beginWord);
-//   const graph = buildGraph(wordList);
-
-//   let nodeBegin = graph.get(beginWord);
-//   let nodeEnd = graph.get(endWord);
-//   if (!nodeEnd) return 0;
-//   nodeBegin.distance = nodeEnd.distance = 1;
-//   nodeBegin.visitedBegin = nodeEnd.visitedEnd = true;
-
-//   const queueBegin = [nodeBegin];
-//   const queueEnd = [nodeEnd];
-
-//   while (queueBegin.length || queueEnd.length) {
-//     nodeBegin = queueBegin.shift() || new GraphNode();
-//     nodeEnd = queueEnd.shift() || new GraphNode();
-
-//     for (let child of nodeBegin.children) {
-//       if (child.visitedEnd) {
-//         return nodeBegin.distance + child.distance;
-//       } else if (!child.visitedBegin) {
-//         child.distance = nodeBegin.distance + 1;
-//         child.visitedBegin = true;
-//         queueBegin.push(child);
-//       }
-//     }
-
-//     for (let child of nodeEnd.children) {
-//       if (child.visitedBegin) {
-//         return nodeEnd.distance + child.distance;
-//       } else if (!child.visitedEnd) {
-//         child.distance = nodeEnd.distance + 1;
-//         child.visitedEnd = true;
-//         queueEnd.push(child);
-//       }
-//     }
-//   }
-
-//   return 0;
-// }
-
-// function buildGraph(wordList) {
-//   const graph = new Graph();
-//   const wordSet = new Set(wordList);
-//   const a = 'a'.charCodeAt(0);
-
-//   for (let word of wordSet) {
-//     for (let i = 0; i < word.length; i++) {
-//       for (let j = 0; j < 26; j++) {
-//         let transWord = word.substring(0, i) + String.fromCharCode(a + j) + word.substr(i + 1);
-//         if (wordSet.has(transWord) && transWord !== word) {
-//           graph.add(word, transWord);
-//         }
-//       }
-//     }
-//   }
-
-//   return graph;
+  getNode(word) {
+    if (!(word in this.graph)) return;
+    return this.graph[word];
+  }
 }
+
+class WordNode {
+  constructor(word) {
+    this.word = word;
+    this.visited = false;
+    this.neighbors = [];
+  }
+}
+
+console.log(ladderLength('hit', 'cog', ["hot", "dot", "dog", "lot", "log", "cog"]));
+console.log(ladderLengthGraphOneDirectional('hit', 'cog', ["hot", "dot", "dog", "lot", "log", "cog"]));
+
+

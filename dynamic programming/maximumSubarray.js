d3e83658a7045f69f8a5c0ae409940cbddab1a96
nehachaudhary <neha@owler-inc.com>
2019-11-11 17:30:53 -0800

    Fix spacing

--- dynamic programming/maximumSubarray.js
+++ dynamic programming/maximumSubarray.js
@@ -18,8 +18,8 @@ const maxSubArray = nums => {
   let maxSum = nums[0]
   let currSum = nums[0];
   for (let i = 1; i < nums.length; i++) {
-      currSum = Math.max(currSum + nums[i], nums[i]);
-      maxSum = Math.max(maxSum, currSum);
+    currSum = Math.max(currSum + nums[i], nums[i]);
+    maxSum = Math.max(maxSum, currSum);
   }
   return maxSum;
 };

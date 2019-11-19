aaa4be84aa179af176bb85bd4850e13433964ea2
nehachaudhary <neha@owler-inc.com>
2019-11-18 18:12:39 -0800

    Clean up knacksack

--- dynamic programming/knapsack.js
+++ dynamic programming/knapsack.js
@@ -27,34 +27,6 @@ var knapsack = (weight, items) => {
   return valueTable[items.length][weight];
 }
 
-//Exponential time solution
-// var knapsack = function (weight, items) {
-//   let maxValue = 0;
-//   let start = 0;
-//   let remainingWeight = weight;
-
-//   const loop = (i, value = 0) => {
-//     if (maxValue < value) {
-//       maxValue = value;
-//     }
-//     for (i; i < items.length; i++) {
-//       let item = items[i];
-//       if (item.w <= remainingWeight) {
-//         value += item.v;
-//         remainingWeight -= item.w;
-//         loop(i + 1, value);
-
-//         value -= item.v;
-//         remainingWeight += item.w;
-//       } else if (remainingWeight > 0) {
-//         continue;
-//       }
-//     }
-//   }
-//   loop(start);
-//   return maxValue;
-// };
-
 let items1 = [{ w: 3, v: 10 }, { w: 2, v: 6 }, { w: 4, v: 11 }]; //max weight 7
 let items2 = [{ w: 12, v: 4 }, { w: 2, v: 2 }, { w: 1, v: 1 }, { w: 1, v: 2 }, { w: 4, v: 10 }]; //// max weight 15
 let items3 = [{ w: 5, v: 10 }, { w: 4, v: 40 }, { w: 6, v: 30 }, { w: 3, v: 50 }]; // max weight 10
